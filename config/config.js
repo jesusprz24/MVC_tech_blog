4c4fcb19799ca8b7abcac44cd36f6e6621e243d7
Jesus <perez.jesus1039@gmail.com>
2023-04-16 15:25:14 -0700

    added comments for future reference

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
+// requires the app to use sequelize and dotenv
 const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
+// creates the connection to the db
 const sequelize = process.env.DATABASE_URL
     ? new Sequelize(process.env.DATABASE_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_PASSWORD, {
