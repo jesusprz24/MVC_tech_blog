0f3826b391f0fd5740f73f09470e3457ba2819d0
Jesus <perez.jesus1039@gmail.com>
2023-04-16 16:32:39 -0700

    added the required dependencies, the get request for root and the post request for path

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,29 @@
+// require router, comment and auth from utils
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    Comment.findAll({})
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req, res) => {
+    console.log(req.body);
+    if (req.session) {
+        Comment.create({
+            text: req.body.text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id,
+        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
+});
