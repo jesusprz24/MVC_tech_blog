4b81844692efc7307fc9f0c1fcddb7109fbaea32
Jesus <perez.jesus1039@gmail.com>
2023-04-16 18:54:18 -0700

    added comment to helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 // code will export object using formate date
 module.exports = {
-// will return a string in localized format
+// will return a string in localized format mm/dd/yyyy
     format_date: (date) => {
         return date.toLocaleDateString();
     },
