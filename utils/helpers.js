6c2f4c09906c6ad415a6166e076868e5aa1a1864
Jesus <perez.jesus1039@gmail.com>
2023-04-16 18:52:16 -0700

    created the helpers to return a string in localized date format

---
+++ utils/helpers.js
@@ -0,0 +1,7 @@
+// code will export object using formate date
+module.exports = {
+// will return a string in localized format
+    format_date: (date) => {
+        return date.toLocaleDateString();
+    },
+};
