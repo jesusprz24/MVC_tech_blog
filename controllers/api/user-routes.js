d1c308dcf64687db6c25f4336ce84caaa85c49cb
Jesus <perez.jesus1039@gmail.com>
2023-04-16 17:08:35 -0700

    post request to for the login created

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -17,3 +17,25 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+router.post('/login', async (req, res) => {
+    try {    
+    const userData = await User.findOne({ where: { username: req.body.username } });
+
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+
+    })
