712489f20a5420dffdf55134ac84d85d329b0fd7
Jesus <perez.jesus1039@gmail.com>
2023-04-16 18:03:18 -0700

    added the user datatypes

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
+    })
+
+    module.exports = User;
